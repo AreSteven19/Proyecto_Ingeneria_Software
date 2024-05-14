70cafba9fcfea63e44d34378202bd94802e98cbd
AreSteven19 <arestevenacevedopereira@gmail.com>
2024-05-14 14:27:00 -0600

    Creacion del proyecto

---
+++ src/components/Login.jsx
@@ -0,0 +1,43 @@
+
+import './estilo.css';
+
+
+
+
+
+
+
+
+
+export function Logininterfaz() {
+
+
+
+
+  return (
+
+    <div className="container-principal">
+
+
+      <div className="formulario">
+
+        <form action="">
+
+          <label>ingrese el usuario:</label>
+          <input type="text" />
+
+          <label>ingrese la constraseña:</label>
+          <input type="text" />
+
+          <button action=''>ingresar</button>
+
+        </form>
+
+      </div>
+
+
+    </div>
+  );
+}
+
+export default Logininterfaz;
