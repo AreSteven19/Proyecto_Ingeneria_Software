adf5950891448a932d14688df629d48072902da8
RolDev-22 <rolandomuagui1629@gmail.com>
2024-06-18 12:02:56 -0600

    Modificaciones de estilos y creación de el componente Servicios para la página principal

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,28 +1,21 @@
-import React from 'react';
-import LoginC from './components/Login.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PaginaR from './Pages/pagina_registro.jsx';
-import Navbar2 from './components/Navbar2.jsx';
-import PaginaP from './Pages/pagina_principal.jsx';
+import React from "react";
+import LoginC from "./components/Login.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PaginaR from "./Pages/pagina_registro.jsx";
+import Navbar2 from "./components/Navbar2.jsx";
+import PaginaP from "./Pages/pagina_principal.jsx";
 function App() {
   return (
     <div className="App">
-
-      
-      
       <BrowserRouter>
         <Routes>
-          
           //para usar las rutas
-
           <Route path="/" element={<LoginC />} />
           <Route path="/pagina_registro" element={<PaginaR />} />
           <Route path="/pagina_principal" element={<PaginaP />} />
           <Route path="/navbar" element={<Navbar2 />} />
         </Routes>
       </BrowserRouter>
-
-     
     </div>
   );
 }
---
+++ Client/src/components/Servicios.js
@@ -0,0 +1,9 @@
+import "../Styles/Style.Servicios.css";
+
+export default function Servicios() {
+  return (
+    <div className="Container_Servicios">
+      <h1>Servicios</h1>
+    </div>
+  );
+}
