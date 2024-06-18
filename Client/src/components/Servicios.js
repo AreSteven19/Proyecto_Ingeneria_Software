c34401b180d5be22baccba66028cff1789f469ce
RolDev-22 <rolandomuagui1629@gmail.com>
2024-06-18 12:31:03 -0600

    Se añade estructura y partes de la pagina de servicios, se modifica el estilo es esta y se realizan ajustes en el estilo de la pagina de Bienvenida

--- Client/src/components/Servicios.js
+++ Client/src/components/Servicios.js
@@ -2,8 +2,54 @@ import "../Styles/Style.Servicios.css";
 
 export default function Servicios() {
   return (
-    <div className="Container_Servicios">
-      <h1>Servicios</h1>
+    <div className="container_Servicios">
+      <div className="container_tittleServicios">
+        <p className="title_Servicios">Nuestros Servicios</p>
+      </div>
+      <div className="container_Cards">
+        <div className="card">
+          <div id="logoCard"></div>
+          <div id="tittleCard">
+            <p>Cortes</p>
+          </div>
+          <div id="descriptionCard">
+            <p>Todo tipo de cortes y sombras</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <div id="logoCard"></div>
+          <div id="tittleCard">
+            <p>Cortes</p>
+          </div>
+          <div id="descriptionCard">
+            <p>Todo tipo de cortes y sombras</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <div id="logoCard"></div>
+          <div id="tittleCard">
+            <p>Cortes</p>
+          </div>
+          <div id="descriptionCard">
+            <p>Todo tipo de cortes y sombras</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <div id="logoCard"></div>
+          <div id="tittleCard">
+            <p>Cortes</p>
+          </div>
+          <div id="descriptionCard">
+            <p>Todo tipo de cortes y sombras</p>
+          </div>
+        </div>
+      </div>
+      <div className="container_Btn">
+        <button className="Button_Bienvenido">Reservar</button>
+      </div>
     </div>
   );
 }
