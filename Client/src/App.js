a09e0d2d290c0895cae601d52932e22e93dda6e8
AreSteven19 <arestevenacevedopereira@gmail.com>
2024-06-19 17:19:41 -0600

    Solucion de conflictos

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,17 +1,11 @@
 
 import React from "react";
-import LoginC from "./components/Login.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PaginaR from "./Pages/pagina_registro.jsx";
 import Navbar2 from "./components/Navbar2.jsx";
 import PaginaP from "./Pages/pagina_principal.jsx";
-
-import React from 'react';
 import LoginC from './components/Login.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PaginaR from './Pages/pagina_registro.jsx';
-import Navbar2 from './components/Navbar2.jsx';
-import PaginaP from './Pages/pagina_principal.jsx';
+
 
 function App() {
   return (
