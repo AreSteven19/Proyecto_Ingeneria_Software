603872d74b42fa34ee2737df45cfda4e52c91a8b
RolDev-22 <rolandomuagui1629@gmail.com>
2024-06-19 21:27:58 -0600

    Cambios en página principal y estylo del componente de bienvenida

--- Client/src/Pages/pagina_principal.jsx
+++ Client/src/Pages/pagina_principal.jsx
@@ -1,16 +1,17 @@
 import "../Styles/EstiloLogin.css";
 import Jumbotron from "../components/Jumbotron";
 import Bienvenido from "../components/Bienvenido";
-import { Navbar2 } from "../components/Navbar2";
+import Navbar2 from "../components/Navbar2";
+import Servicios from "../components/Servicios";
 
 export function PaginaP() {
   return (
     <div className="contenido" style={{ height: "100%" }}>
-      <Navbar2></Navbar2>
+      <Navbar2 />
       <div className="bodyPrincipal" style={{ height: "calc(100vh - 90px)" }}>
         <Jumbotron />
         <Bienvenido />
-        {/* <Servicios /> */}
+        <Servicios />
       </div>
     </div>
   );
